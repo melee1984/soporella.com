70a9c37d43e55599ad552759d50a4b92e656871b
Leslie Parba <lparba@gmail.com>
2021-03-01 14:29:08 +0800

    Adding install.sh shell command

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ echo "===========  Installation Starting =============";
 
 echo "-----------  Install front-end dependencies ------";
 # sudo npm install --global gulp-cli
-# npm install
+npm install
 
 echo "-----------  Installing composer dependencies ------";
-# composer install -q -n
+composer install -q -n
 
 echo "-----------  Migrating the database ------";
 echo "-----------  Seeding the database ------";
