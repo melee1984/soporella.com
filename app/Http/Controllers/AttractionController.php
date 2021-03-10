cd83ae9377adcba86041108a54fd1eaeae4cc313
Leslie Parba <lparba@gmail.com>
2021-03-10 13:52:15 +0800

    updating

--- app/Http/Controllers/AttractionController.php
+++ app/Http/Controllers/AttractionController.php
@@ -10,8 +10,6 @@ class AttractionController extends Controller
 {
 	public function index(Category $category, Attraction $attraction) 
     {   
-       echo "Attraction COntroller";
-       
        return view('front.pages.listing');
     }
 }
