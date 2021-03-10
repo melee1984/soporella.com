3cba8ee3304a92c8ebd020e45caa22b4020975fb
Leslie Parba <lparba@gmail.com>
2021-03-10 11:26:54 +0800

    no message

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,11 +16,9 @@ class DatabaseSeeder extends Seeder
         $this->call(AttractionsSeeder::class);
         $this->call(CategoriesSeeder::class);
         $this->call(TopAttractionSeeder::class);
-        
-        $this->call(VisitSeeder::class);
-        $this->call(CampaignSeeder::class);
         $this->call(PromotionSeeder::class);
-        
+        $this->call(CampaignSeeder::class);
+        $this->call(VisitSeeder::class);
     }
 }
 // php artisan make:seeder VisitSeeder
