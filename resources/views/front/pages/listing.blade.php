05c26e269b1f36018d1fd4887ee2c6df298080de
Leslie Parba <lparba@gmail.com>
2021-03-10 13:00:29 +0800

    removing jumbotron

--- resources/views/front/pages/listing.blade.php
+++ resources/views/front/pages/listing.blade.php
@@ -4,11 +4,11 @@
 	
 <div class="container" id="top">
 
-	<div class="jumbotron">
+	<!-- <div class="jumbotron">
 		  <h3>Best Safety Standards!</h3>
 		  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
 		  <p><a class="btn buy" href="#" role="button">Learn more</a></p>
-	</div>
+	</div> -->
 
 	@include('front.pages.includes.display', 
 		['title' => $category->title , 'attractions' => $category->attractionsMapping])
