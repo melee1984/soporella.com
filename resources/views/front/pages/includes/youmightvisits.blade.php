9e79bc774752d62342fd16e04746395a0401f91e
Leslie Parba <lparba@gmail.com>
2021-03-10 11:38:09 +0800

    no message

--- resources/views/front/pages/includes/youmightvisits.blade.php
+++ resources/views/front/pages/includes/youmightvisits.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-12">
-		<h3>You might also want to visit...</h3>
+		<h3>{{ __('You might also want to visit...') }}</h3>
 	</div>
 	<div class="row tab-content padding-20">
 		@foreach($suggestionAttractions as $attra)
@@ -8,9 +8,9 @@
 		 <a href="{{ route('page.visit', $attra->attraction ) }}">
 	        <img src="{{ $attra->attraction->photo }}" alt="{{ $attra->attraction->title }}" class="img-responsive">
 	      </a>
-          <h4><a href="{{ route('page.visit', $attra->attraction ) }}" title="{{ $attra->attraction->title }}">{{ $attra->attraction->title }}</a></h4>
-          <p>{{ Str::words($attra->attraction->description, 20) }} <a href="{{ $attra->attraction->pageUrl }}"> more</a></p>
-          <a class="buy" href="{{ route('page.visit', $attra->attraction ) }}">Buy Tickets</a>
+	      <h4><a href="{{ route('page.visit', $attra->attraction ) }}" title="{{ $attra->attraction->title }}">{{ $attra->attraction->title }}</a></h4>
+	      <p>{{ Str::words($attra->attraction->description, 20) }} <a href="{{ $attra->attraction->pageUrl }}"> more</a></p>
+	      <a class="buy" href="{{ route('page.visit', $attra->attraction ) }}">Buy Tickets</a>
 		</div>
 		@endforeach
 	</div>
