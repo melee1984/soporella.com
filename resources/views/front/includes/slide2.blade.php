956132812237ccb8cd68dbb1ec56ccb7eb5cc67d
Leslie Parba <lparba@gmail.com>
2021-05-09 16:37:56 +0800

    Updating text limit

--- resources/views/front/includes/slide2.blade.php
+++ resources/views/front/includes/slide2.blade.php
@@ -9,7 +9,7 @@
         <div class="col-md-6 text-center">
             <div class="carousel-caption">
               <h2>{{ $campaign->attraction->title }}</h2>
-              <p>{{ $campaign->attraction->language_string['description'] }}</p>
+              <p>{{ Str::words($campaign->attraction->language_string['description'] , 30) }} </p>
               <a class="buy" href="{{ route('page.promotion', $campaign->attraction) }}">{{ trans('messages.LABEL_BUY_TICKET') }}</a>
             </div>
         </div>    
