5505bbf4e94044446c98d7492c58688929397383
Leslie Parba <lparba@gmail.com>
2021-06-04 13:08:34 +0800

    added

--- resources/views/front/template/default.blade.php
+++ resources/views/front/template/default.blade.php
@@ -5,7 +5,7 @@
     @include('front.includes.meta')
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="http://soporella.com.test/assets/css/vendors/flag-icon.css">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/flag-icon.css') }}">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
