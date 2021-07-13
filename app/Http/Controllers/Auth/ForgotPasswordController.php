36842f19ac2e26cb644f9a7625ad30ef75fb3e48
Leslie Parba <lparba@gmail.com>
2021-07-13 14:17:12 +0800

    Added

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
+use Cache;
+use App\Category;
+use Str;
+
 class ForgotPasswordController extends Controller
 {
     /*
@@ -19,4 +23,20 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+    public function showLinkRequestForm()
+    {   
+        $menus = Cache::remember('menus', 30, function () {
+           $menus = Category::forMenu()->active()->get();
+
+            foreach ($menus as $category) {
+                $category->language_string = $category->convertLanguageField();
+            }
+            return $menus;
+        });
+            
+        return view('auth.passwords.email', compact('menus'));
+
+    }
+
 }
